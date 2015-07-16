e06f7c1da45db165cdcab02201724d0a77a656ee
Wevers Santos <weverss@gmail.com>
2015-07-15 21:01:49 -0300

    Module skeleton

---
+++ README.rst
@@ -0,0 +1,5 @@
+============
+vmod_includereq
+============
+
+Varnish vmod for include additional http response bodies into the original request response.
---
+++ src/vmod_includereq.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "cache/cache.h"
+
+#include "vcc_if.h"
+
+int
+init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
+{
+	return (0);
+}
